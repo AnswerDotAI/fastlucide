505b802cef39d44ced3007dc9c58d529b5616319
Jeremy Howard <github@jhoward.fastmail.fm>
2025-11-25 14:47:23 +1000

    refactor and docs

--- fastlucide/icons.py
+++ fastlucide/icons.py
@@ -1,7 +1,7 @@
 from .core import SvgSprites
 from functools import partial
 
-__all__ = ['spritesheet']
+__all__ = ['spritesheet','combined']
 
 spritesheet = SvgSprites()
 
@@ -12,7 +12,11 @@ def __getattr__(nm):
     spritesheet.nms.add(nm)
     return partial(spritesheet, nm)
 
+def combined(nm1, nm2, position='overlap', sz=None, 
+             stroke1=None, fill1=None, stroke2=None, fill2=None, **kw):
+    return spritesheet.combined(nm1, nm2, position, sz, 
+             stroke1=stroke1, fill1=fill1, stroke2=stroke2, fill2=fill2, **kw)
+
 def __dir__():
     return [(o[0].upper()+o[1:]).replace('-','_')
             for o in spritesheet.icons]
-
